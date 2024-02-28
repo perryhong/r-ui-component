2dec319412944d09827759521507a599c2d2c8fd
Peiqin Hong <peiqin.hong@shopee.com>
2024-02-28 12:25:31 +0800

    feat: 打包支持umd

---
+++ rollup.config.mjs
@@ -0,0 +1,62 @@
+import typescript from "rollup-plugin-typescript2";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commmonJs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import sass from "rollup-plugin-sass";
+// import excludeDependencies from "rollup-plugin-exclude-dependencies-from-bundle";
+import { terser } from "rollup-plugin-terser";
+import replace from "@rollup/plugin-replace";
+
+const overrides = {
+  compilerOptions: {
+    declaration: true,
+  },
+  exclude: [
+    "src/**/*.test.tsx",
+    "src/**/*.stories.ts",
+    "src/**/*.stories.tsx",
+    "src/**/*.stories.mdx",
+    "src/**/*.setupTests.ts",
+  ],
+};
+
+const config = {
+  input: "src/index.tsx",
+  output: [
+    {
+      file: "dist/index.es.js",
+      format: "es",
+    },
+    {
+      file: "dist/index.min.js",
+      format: "es",
+      plugins: [terser()],
+    },
+    {
+      name: "RecComponent",
+      file: "dist/index.umd.js",
+      format: "umd",
+      exports: "named",
+      globals: {
+        react: "React",
+        "react-dom": "ReactDOM",
+        axios: "Axios",
+      },
+      plugins: [terser()],
+    },
+  ],
+  plugins: [
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
+    nodeResolve(),
+    commmonJs(),
+    json(),
+    // excludeDependencies(),
+    typescript({ tsconfigOverride: overrides }),
+    sass({ output: "dist/index.css" }),
+  ],
+  external: ["react", "react-dom", "axios"],
+};
+
+export default config;
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,3 @@
+import "./styles/index.scss";
+
 export { default as Button } from "./components/Button";
